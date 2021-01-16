f4f05b1349c59ed359c1c875bc769f2919c95188
Isaac Rodriguez <64040285+rdzisaac23@users.noreply.github.com>
2021-01-15 22:08:06 -0500

    index week1

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const links = [{
+        label: "Week1 notes",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week2",
+        url: "#"
+    },
+    {
+        label: "Challenge 1: Todo",
+        url: "#"
+    }
+]
+
+function orderList() {
+    
+    for (var i = 0; i < links.length; i++){
+        //create a
+         
+        //create li items
+        let a = document.createElement('a');
+        let list = document.createElement('li');
+    
+        // Set contents
+        a.textContent = links[i].label;
+        a.setAttribute('href', links[i].url);
+    
+        // add them to the ol
+        list.appendChild(a);
+        console.log(list);
+
+        document.querySelector('ol#order-list').appendChild(list);  
+    }
+      
+};
+
+
+links.forEach(orderList => {
+});
+
+console.log(links);
+orderList();
